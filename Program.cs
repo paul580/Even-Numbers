781244969fc342ab9c67598f3dc4f619a2055742
Paul <paul18346@gmail.com>
2020-02-20 13:21:02 -0700

    Escribimos los numeros en pares

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+namespace Even_Numbers
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            
+
+            
+
+            for(int asd=0; asd<11; asd++)
+
+			{
+
+            if((asd % 2) == 0 ){
+
+			Console.WriteLine(asd);
+
+            }
+
+            }
+
+			
+
+        }
+
+    }
+
+}
